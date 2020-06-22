5b878456fc63e377383557e7f984e9d0a0df9388
Friska Sianturi <friskasianturi23@gmail.com>
2020-06-22 15:33:52 +0700

    Modal part I design

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,25 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
 
-require('./bootstrap');
+// require('./bootstrap');
 
-window.Vue = require('vue');
+// window.Vue = require('vue');
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+// Vue.component('theme-swither', require('./components/ThemeSwither.vue').default);
 
-Vue.component('theme-swither', require('./components/ThemeSwither.vue').default);
+// const app = new Vue({
+//     el: '#app',
+// });
+import './bootstrap';
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+import Vue from 'vue';
+import VModal from 'vue-js-modal';
 
-const app = new Vue({
-    el: '#app',
+Vue.use(VModal);
+
+Vue.component('theme-switcher', require('./components/ThemeSwitcher.vue').default);
+//Vue.component('new-project-modal', require('./components/NewProjectModal.vue').default);
+//Vue.component('dropdown', require('./components/Dropdown.vue').default);
+
+new Vue({
+    el: '#app'
 });
